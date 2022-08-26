9b8f35793f6e5bffa10f35fa1690f0b6877454e3
thiago.machado <thiago.machado@seati.ma.gov.br>
2022-08-26 12:26:07 -0300

    criando tema light

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './routes';
+import { LightTheme } from './shared/themes';
 
 export const App = () => {
   return (
-    <div className="App">
-      MOB - Credenciamento
-    </div>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export{}
+import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes =() => {
+
+    return (
+    <Routes>
+        <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' >Entrar</Button>} />
+
+        <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+    );
+}
