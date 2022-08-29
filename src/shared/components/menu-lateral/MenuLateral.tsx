d3cc960e2d0caee6989f2f96affa8775330273f4
Thiago Machado de Araújo <thiago.machado@seati.ma.gov.br>
2022-08-29 13:05:54 -0300

    imagem

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Divider, Drawer, useTheme } from "@mui/material"
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from '@mui/material';
 
 interface IAppThemeProviderProps {
     children: React.ReactNode
@@ -9,20 +9,29 @@ export const MenuLateral: React.FC<IAppThemeProviderProps> = ({ children }) =>{
     const theme =useTheme();
     return (
         <>
-            <Drawer variant='permanent'>
+            <Drawer variant="permanent">
                 <Box width={theme.spacing(28)}>
                     <Box width='100%' height={theme.spacing(15)} display="flex" alignItems="center"  justifyContent="center">
                         <Avatar 
                         sx={{heigth: theme.spacing(12), width: theme.spacing(6)}}
-                        src="../images/LAMPADA SUPER LED ALTA H1.jpg"
+                        src="https://www.pngmart.com/files/7/Domestic-Robot-PNG-Clipart.png"
                         />                        
                     </Box>
-                    <Box width='100%' height={theme.spacing(2)} display="flex" alignItems="center"  justifyContent="center">
+                    <Box width='100%' height={theme.spacing(6)} display="flex" alignItems="center"  justifyContent="center">
                             Nome do Usuario
                     </Box>
                     <Divider />
                     <Box flex={1}>
-
+                        <List>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Icon>
+                                        home
+                                    </Icon>
+                                </ListItemIcon>
+                                <ListItemText primary = "Pagina inicail" />
+                            </ListItemButton>
+                        </List> 
                     </Box>
                 </Box>
             </Drawer>
